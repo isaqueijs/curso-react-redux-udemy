09e61ce3c77dafe4928724d1397481f45955924b
Isaque Joao Da Silva <ijsilva1@stefanini.com>
2020-03-05 13:18:50 -0300

    editando index.html projeto exercícios-react --- Olá React, aula 9

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -2,4 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 
 const elemento = document.getElementById("root");
-ReactDOM.render(<h1>Olá React!!</h1>, elemento)
+const jsx = <h1>Olá React!</h1>
+ReactDOM.render(jsx, elemento)
+
+
+// ReactDOM.render(
+//     <ul>
+//         <li>1) Predro</li>
+//         <li>2) Maria</li>
+//         <li>3) ana</li>
+//     </ul>    
+// , elemento)
