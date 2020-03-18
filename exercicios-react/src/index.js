d80e18c04a52d966aa52098180feb7f30c306864
unknown <suporte@STFRECC031789-W.stefanini.dom>
2020-03-18 14:12:21 -0300

    comunicação direta entre componentes - aula 20

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -4,15 +4,21 @@ import ReactDOM from 'react-dom';
 // import { CompA, CompB as B} from './componentes/DoisComponentes'
 // import MultiElementos from './componentes/MultiElementos'
 import FamiliaSilva from './componentes/FamiliaSilva'
+import Familia from './componentes/Familia';
+import Membro from './componentes/Membro'
 
 const elemento = document.getElementById("root");
 ReactDOM.render(
     <div>
+        <Familia sobrenome="Pereira" >
+            <Membro nome="Andre"/>
+            <Membro nome="Mariana"/>
+        </Familia>
+        <FamiliaSilva/>
         {/* <MultiElementos /> */}
         {/* <CompA valor="Olá eu sou o A!" />
         <B valor="B na área!" /> */}
         {/* <PrimeiroComponente valor="Bom dia!" idade={25} /> */}
-        <FamiliaSilva />
     </div>
 , elemento)
 
